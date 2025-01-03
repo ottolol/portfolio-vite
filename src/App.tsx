42e93d077927766d20a249a4795b81771e687b5a
ottolol <alexsveshnik@gmail.com>
2025-01-03 13:59:06 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import './App.css'
-import styled from "styled-components";
+import "./App.css";
+import { Footer } from "./layout/footer/Footer";
+import { Header } from "./layout/header/Header";
+import { AboutMe } from "./layout/sections/aboutMe/AboutMe";
+import { Contacts } from "./layout/sections/contacts/Contacts";
+import { Projects } from "./layout/sections/projects/Projects";
+import { Skills } from "./layout/sections/skills/Skills";
 
 function App() {
-    return (
-        <div className="App">
-            <Title>Welcome to IT-INCUBATOR</Title>
-        </div>
-    )
+  return (
+    <div className="App">
+      <Header />
+      <AboutMe />
+      <Projects />
+      <Skills />
+      <Contacts />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
-
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
+export default App;
