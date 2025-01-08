9def6c49116ca80c0ac836615aa09b3f7fe64707
ottolol <alexsveshnik@gmail.com>
2025-01-08 22:14:58 +0300

    add Theme.ts

---
+++ src/styles/Theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    primaryBg: "#fafafa",
+    seconradyBg: "#F5F2FD",
+    accent: "#5222D0",
+
+    titles: "#151717",
+    buttons: "#DBFFFF",
+    warnings: "#C05353",
+    font: "#1F2626",
+
+    inputBorder: "#534084",
+    inputPlaceholder: "#949b91",
+    inputFocusVisible: "#5222d0",
+  },
+};
