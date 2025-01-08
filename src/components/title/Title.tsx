cbd42af32cd6fc1574c309ac2009a0481eb9d2e9
ottolol <alexsveshnik@gmail.com>
2025-01-08 22:17:15 +0300

    update Title.tsx

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -4,4 +4,8 @@ export function Title(props: { title: string }) {
   return <StyledTitle>{props.title}</StyledTitle>;
 }
 
-const StyledTitle = styled.h2``;
+const StyledTitle = styled.h2`
+  font-size: 24px;
+  font-weight: 600;
+  letter-spacing: 0.04em;
+`;
