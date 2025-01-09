b34abcd8f48a1f78ea56439a352ad95e45bc7136
ottolol <alexsveshnik@gmail.com>
2025-01-09 20:37:46 +0300

    add css style to Skill.tsx

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -4,16 +4,41 @@ import { Icon } from "../../../../components/icon/Icon";
 type SkillPropsType = {
   iconId: string;
   text: string;
+  width?: string;
+  height?: string;
+  viewBox?: string;
 };
 
 export function Skill(props: SkillPropsType) {
   return (
     <StyledSkill>
-      <Icon iconId={props.iconId} />
+      <Icon
+        iconId={props.iconId}
+        width={props.width}
+        height={props.height}
+        viewBox={props.viewBox}
+      />
       <Text>{props.text}</Text>
     </StyledSkill>
   );
 }
 
-const StyledSkill = styled.div``;
-const Text = styled.div``;
+const StyledSkill = styled.li`
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 60px;
+
+  /* &:last-child {
+    margin-bottom: 0px;
+  } */
+`;
+const Text = styled.span`
+  font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 0.1354em;
+  text-transform: uppercase;
+  margin-top: 20px;
+  text-align: center;
+`;
