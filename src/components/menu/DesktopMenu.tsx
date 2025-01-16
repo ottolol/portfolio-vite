17791819a8fb00bee5c937111b15c2db674e506f
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-01-16 13:54:21 +0000

    add DesktopMenu.tsx

---
+++ src/components/menu/DesktopMenu.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import { theme } from "../../styles/Theme";
+import { font } from "../../styles/Common";
+
+type menuPropsType = {
+  menuItems: Array<string>;
+  className?: string;
+};
+
+export function Menu(props: menuPropsType) {
+  return (
+    <MenuStyled className={props.className}>
+      <ul>
+        {props.menuItems.map((item) => {
+          return (
+            <li key={item}>
+              <a href="#">{item}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </MenuStyled>
+  );
+}
+
+const MenuStyled = styled.nav`
+  ul {
+    display: flex;
+    gap: 67px;
+    list-style: none;
+    justify-content: space-between;
+
+    padding: 40px 0;
+  }
+
+  li {
+    font-size: 16px;
+    font-weight: 500;
+  }
+
+  a {
+    color: ${theme.colors.font};
+    padding: 12px 20px;
+
+    &:hover {
+      color: ${theme.colors.buttons};
+      background-color: ${theme.colors.accent};
+      border-radius: 6px;
+    }
+  }
+`;
