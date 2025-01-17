6432258cb017bf1ca1b859e0435aeb85d8338de7
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-01-17 08:32:11 +0000

    add Footer_Styles.ts

---
+++ src/layout/footer/Footer_Styles.ts
@@ -0,0 +1,59 @@
+import { styled } from "styled-components";
+import { theme } from "../../styles/Theme";
+
+const Footer = styled.section`
+  margin-top: 190px;
+
+  @media ${theme.media.mobile} {
+    margin-top: 150px;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 73px;
+`;
+
+const Link = styled.a`
+  font-size: 12px;
+  font-weight: 400;
+  letter-spacing: 0.12em;
+  color: ${theme.colors.font};
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  span {
+    margin-top: 10px;
+  }
+`;
+
+const BottomMenuStyles = styled.div`
+  margin: 9px 0;
+
+  .bottom-menu li {
+    font-size: 14px;
+    font-weight: 400;
+  }
+`;
+
+const Copyright = styled.small`
+  font-size: 14px;
+  font-weight: 400;
+  letter-spacing: 0.04em;
+
+  text-transform: uppercase;
+`;
+
+export const S = {
+    Footer,
+    Wrapper,
+    Link,
+    BottomMenuStyles,
+    Copyright
+}
