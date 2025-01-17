3e7a0167cc2d803d9afc161f24fb6d1cb4e3e283
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-01-17 09:23:02 +0000

    remove styles to Contacts_Styles.ts

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,120 +1,27 @@
-import styled from "styled-components";
 import { Title } from "../../../components/title/Title";
-import { theme } from "../../../styles/Theme";
+import { S } from "./Contacts_Styles"
 
-export function Contacts() {
+export const Contacts: React.FC = () => {
   return (
-    <StyledContacts>
+    <S.Contacts>
       <Title title="Contacts"></Title>
-      <Form action="" method="">
-        <Field>
+      <S.Form action="" method="">
+        <S.Field>
           <label>Name</label>
           <input placeholder="Имя" type="text" />
-        </Field>
-        <Field>
+        </S.Field>
+        <S.Field>
           <label>Email</label>
           <input placeholder="123@gmail.com" type="email" />
-        </Field>
-        <Field>
+        </S.Field>
+        <S.Field>
           <label>Message</label>
-          <InputArea as="textarea" placeholder="Введите текс" />
-        </Field>
-        <ButtonSubmit type="submit">Submit</ButtonSubmit>
-      </Form>
-    </StyledContacts>
+          <S.InputArea as="textarea" placeholder="Введите текс" />
+        </S.Field>
+        <S.ButtonSubmit type="submit">Submit</S.ButtonSubmit>
+      </S.Form>
+    </S.Contacts>
   );
 }
 
-const StyledContacts = styled.section`
-  margin-top: 100px;
 
-  @media ${theme.media.mobile} {
-    margin-top: 18px;
-  }
-`;
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  width: max-content;
-  margin: 0 auto;
-  gap: 10px;
-
-  max-width: 532px;
-  width: 100%;
-`;
-
-const Field = styled.div`
-  flex-direction: column;
-  display: flex;
-
-  label {
-    font-size: 14px;
-    font-weight: 400;
-    display: inline-block;
-    margin: 20px 0 6px;
-  }
-
-  input {
-    font-size: 16px;
-    font-weight: 500;
-
-    color: ${theme.colors.font};
-    padding: 15px 20px;
-
-    border-radius: 6px;
-    border: 1px solid ${theme.colors.inputBorder};
-
-    &::placeholder {
-      font-size: 14px;
-      font-weight: 400;
-      line-height: 17.07px;
-      color: ${theme.colors.inputPlaceholder};
-    }
-
-    &:focus-visible {
-      outline: 2px solid ${theme.colors.inputFocusVisible};
-    }
-  }
-`;
-
-const InputArea = styled.input`
-  resize: none;
-  height: 165px;
-  font-size: 16px;
-  font-weight: 500;
-
-  color: ${theme.colors.font};
-  padding: 15px 20px;
-
-  border-radius: 6px;
-  border: 1px solid #534084;
-
-  &::placeholder {
-    font-size: 14px;
-    font-weight: 400;
-    color: #949b91;
-  }
-
-  &:focus-visible {
-    outline: 2px solid #5222d0;
-  }
-`;
-
-const ButtonSubmit = styled.button`
-  margin-top: 20px;
-
-  font-size: 16px;
-  font-weight: 500;
-
-  color: ${theme.colors.buttons};
-  background-color: ${theme.colors.accent};
-  border-radius: 6px;
-  padding: 12px 20px;
-  cursor: pointer;
-
-  &:hover {
-    color: ${theme.colors.font};
-    background-color: ${theme.colors.primaryBg};
-    outline: 1px solid ${theme.colors.accent};
-  }
-`;
