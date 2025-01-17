e600d9c8259a1d8fd06ef2e35cde3fe8c7134adf
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-01-17 12:23:16 +0000

    remove styles from Skill.tsx to Skills_Styles.ts

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,7 +1,5 @@
-import styled from "styled-components";
 import { Icon } from "../../../../components/icon/Icon";
-import { theme } from "../../../../styles/Theme";
-import { font } from "../../../../styles/Common";
+import { S } from "./../Skills_Styles"
 
 type SkillPropsType = {
   iconId: string;
@@ -11,47 +9,18 @@ type SkillPropsType = {
   viewBox?: string;
 };
 
-export function Skill(props: SkillPropsType) {
+export const Skill: React.FC<SkillPropsType> = (props: SkillPropsType) => {
   return (
-    <StyledSkill>
+    <S.Skill>
       <Icon
         iconId={props.iconId}
         width={props.width}
         height={props.height}
         viewBox={props.viewBox}
       />
-      <Text>{props.text}</Text>
-    </StyledSkill>
+      <S.Text>{props.text}</S.Text>
+    </S.Skill>
   );
 }
 
-const StyledSkill = styled.li`
-  width: 20%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 60px;
 
-  @media ${theme.media.tablet} {
-    width: 33%;
-    margin-bottom: 82px;
-  }
-
-  svg {
-    @media ${theme.media.mobile} {
-      width: 85px;
-      height: auto;
-    }
-  }
-`;
-const Text = styled.span`
-  ${font({ weight: 400, Fmax: 16, Fmin: 11 })};
-  letter-spacing: 0.1354em;
-  text-transform: uppercase;
-  margin-top: 20px;
-  text-align: center;
-
-  @media ${theme.media.mobile} {
-    margin-top: 16px;
-  }
-`;
