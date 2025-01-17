9aa1abeef3242c53d6671ccbe9f606a3dd16653a
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-01-17 09:58:30 +0000

    remove styles to Projects_Styles.ts

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -1,21 +1,20 @@
 import React from "react";
-import styled from "styled-components";
 import { Project } from "./project/Project";
-import ProjectImg from "../../../assets/images/project-img.webp";
 import { Title } from "../../../components/title/Title";
-import { theme } from "../../../styles/Theme";
+import ProjectImg from "../../../assets/images/project-img.webp";
+import { S } from "./Projects_Styles"
 
-export function Projects() {
+export const Projects: React.FC = () => {
   const [showAllProjects, SetShowAllProjects] = React.useState(false);
 
   const [width, setWidth] = React.useState(window.innerWidth);
-    const breakpoint = 576;
-  
-    React.useEffect(() => {
-      const handleWindowResize = () => setWidth(window.innerWidth)
-      window.addEventListener("resize", handleWindowResize);
-      return () => window.removeEventListener("resize", handleWindowResize);
-    }, []);
+  const breakpoint = 576;
+
+  React.useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth)
+    window.addEventListener("resize", handleWindowResize);
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
 
   const projectItems = [
     {
@@ -41,9 +40,9 @@ export function Projects() {
   ]
 
   return (
-    <StyledProjects>
+    <S.Projects>
       <Title title="Projects"></Title>
-      <ListProjects>
+      <S.ListProjects>
         {projectItems.slice(0, showAllProjects ? projectItems.length : 2).map((p, index) => ( 
           <Project 
             key={index}
@@ -52,55 +51,16 @@ export function Projects() {
             img={p.img}
           /> 
         ))} 
-      </ListProjects>
+      </S.ListProjects>
       
         {width < breakpoint && !showAllProjects ? 
-          <SeeAllProjectsWrapper>
-            <SeeAllProjects onClick={() => SetShowAllProjects(!showAllProjects)}>
+          <S.SeeAllProjectsWrapper>
+            <S.SeeAllProjects onClick={() => SetShowAllProjects(!showAllProjects)}>
               See all Projets
-            </SeeAllProjects>
-          </SeeAllProjectsWrapper> : ''
+            </S.SeeAllProjects>
+          </S.SeeAllProjectsWrapper> : ''
         }
       
-    </StyledProjects>
+    </S.Projects>
   );
 }
-
-const StyledProjects = styled.section``;
-
-const ListProjects = styled.ul`
-  margin-top: 32px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 25px;
-  
-  @media ${theme.media.mobile} {
-  gap: 16px;}
-`;
-
-const SeeAllProjectsWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-
-  @media ${theme.media.mobile} {
-    padding-top: 36px;
-  }
-`
-
-const SeeAllProjects = styled.button`
-  font-size: 16px;
-  font-weight: 500;
-  text-transform: uppercase;
-
-  color: ${theme.colors.buttons};
-  background-color: ${theme.colors.accent};
-  border-radius: 6px;
-  padding: 18px 73px;
-
-  &:hover {
-    color: ${theme.colors.font};
-    background-color: ${theme.colors.primaryBg};
-    outline: 1px solid ${theme.colors.accent};
-  }
-`
