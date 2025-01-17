ac8acc4f87e66de9590fa779bb006f68ba5c86a5
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-01-17 09:11:35 +0000

    remove styles to About_Styles.ts

--- src/layout/sections/aboutMe/AboutMe.tsx
+++ src/layout/sections/aboutMe/AboutMe.tsx
@@ -1,38 +1,18 @@
-import styled from "styled-components";
 import { Title } from "../../../components/title/Title";
-import { theme } from "../../../styles/Theme";
+import { S } from "./AboutMe_Styles"
 
-export function AboutMe() {
+export const AboutMe: React.FC = () => {
   return (
-    <StyledAboutMe>
+    <S.AboutMe>
       <Title title="About me"></Title>
-      <Text>
+      <S.Text>
         The long barrow was built on land previously inhabited in the Mesolithic
         period. It consisted of a sub-rectangular earthen tumulus, estimated to
         have been 15 metres (50 feet) in length, with a chamber built from
         sarsen megaliths on its eastern end. Both inhumed and cremated human
         remains were placed within this chamber during the Neolithic period,
         representing at least nine or ten individuals.
-      </Text>
-    </StyledAboutMe>
+      </S.Text>
+    </S.AboutMe>
   );
 }
-
-const StyledAboutMe = styled.section`
-  margin-top: 220px;
-
-  @media ${theme.media.mobile} {
-    margin-top: 70px;
-  }
-`;
-
-const Text = styled.p`
-  font-size: 16px;
-  font-weight: 400;
-  letter-spacing: 0.04em;
-  text-align: justify;
-
-  margin-top: 20px;
-  border-left: 5px solid #5222d0;
-  padding-left: 17px;
-`;
