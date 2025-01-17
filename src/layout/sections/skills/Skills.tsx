2222adbd311a8bda763a80c18925da037cdf8e77
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-01-17 12:22:36 +0000

    update Skills.tsx and remove styles to Skills_Styles.ts

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -1,117 +1,40 @@
-import styled from "styled-components";
 import { Skill } from "./skill/Skill";
 import { Title } from "../../../components/title/Title";
-import { theme } from "../../../styles/Theme";
+import { S } from "./Skills_Styles"
 
-export function Skills() {
+export const Skills: React.FC = () => {
+  const skillsItems = [
+    {iconId:"javaScript", text:"Javascript", width:"116", height:"120", viewBox:"0 0 116 120"},
+    {iconId:"typeScript", text:"typescript"},
+    {iconId:"mongoDb", text:"mongo db"},
+    {iconId:"PostgreSql", text:"PostgreSQL"},
+    {iconId:"jest", text:"Jest"},
+    {iconId:"expressJs", text:"express Js"},
+    {iconId:"nestJs", text:"nest Js"},
+    {iconId:"docker", text:"docker"},
+    {iconId:"reactJs", text:"react Js"},
+    {iconId:"reactNative", text:"react Native"},
+    {iconId:"styledComponents", text:"styled Components"},
+    {iconId:"redux", text:"redux"},
+    {iconId:"git", text:"git", viewBox:"0 0 32 32"},
+  ]
+
   return (
-    <StyledSkills>
+    <S.Skills>
       <Title title="Skills" />
-      <Wrapper>
-        <Skill
-          iconId="javaScript"
-          text="Javascript"
-          width="116"
-          height="120"
-          viewBox="0 0 116 120"
-        />
-        <Skill
-          iconId="typeScript"
-          text="typescript"
-          width="120"
-          height="120"
-          viewBox="0 0 120 120"
-        />
-        <Skill
-          iconId="mongoDb"
-          text="mongo db"
-          width="120"
-          height="120"
-          viewBox="0 0 120 120"
-        />
-        <Skill
-          iconId="PostgreSql"
-          text="PostgreSQL"
-          width="120"
-          height="120"
-          viewBox="0 0 120 120"
-        />
-        <Skill
-          iconId="jest"
-          text="Jest"
-          width="120"
-          height="120"
-          viewBox="0 0 120 120"
-        />
-        <Skill
-          iconId="expressJs"
-          text="express Js"
-          width="120"
-          height="120"
-          viewBox="0 0 120 120"
-        />
-        <Skill
-          iconId="nestJs"
-          text="nest Js"
-          width="120"
-          height="120"
-          viewBox="0 0 120 120"
-        />
-        <Skill
-          iconId="docker"
-          text="docker"
-          width="120"
-          height="120"
-          viewBox="0 0 120 120"
-        />
-        <Skill
-          iconId="reactJs"
-          text="react Js"
-          width="120"
-          height="120"
-          viewBox="0 0 120 120"
-        />
-        <Skill
-          iconId="reactNative"
-          text="react Native"
-          width="120"
-          height="120"
-          viewBox="0 0 120 120"
-        />
-        <Skill
-          iconId="styledComponents"
-          text="styled Components"
-          width="120"
-          height="120"
-          viewBox="0 0 120 120"
-        />
-        <Skill
-          iconId="redux"
-          text="redux"
-          width="120"
-          height="120"
-          viewBox="0 0 120 120"
-        />
-        <Skill
-          iconId="git"
-          text="git"
-          width="120"
-          height="120"
-          viewBox="0 0 32 32"
-        />
-      </Wrapper>
-    </StyledSkills>
+      <S.Wrapper>
+        {skillsItems.map((s) => {
+          return (
+            <Skill
+              iconId={s.iconId}
+              text={s.text}
+              width={s.width}
+              height={s.height}
+              viewBox={s.viewBox}
+            />
+          )
+        })}
+      </S.Wrapper>
+    </S.Skills>
   );
 }
-
-const StyledSkills = styled.section``;
-const Wrapper = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  margin-top: 30px;
-
-  @media ${theme.media.mobile} {
-    margin-top: 46px;
-  }
-`;
