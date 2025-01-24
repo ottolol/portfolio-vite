3533cf315225dd5aaabf0a293f5c97df4e596b0d
ottolol <alexsveshnik@gmail.com>
2025-01-24 18:51:41 +0300

    add id to scroll for menu

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -1,26 +1,32 @@
 import { Skill } from "./skill/Skill";
 import { Title } from "../../../components/title/Title";
-import { S } from "./Skills_Styles"
+import { S } from "./Skills_Styles";
 
 export const Skills: React.FC = () => {
   const skillsItems = [
-    {iconId:"javaScript", text:"Javascript", width:"116", height:"120", viewBox:"0 0 116 120"},
-    {iconId:"typeScript", text:"typescript"},
-    {iconId:"mongoDb", text:"mongo db"},
-    {iconId:"PostgreSql", text:"PostgreSQL"},
-    {iconId:"jest", text:"Jest"},
-    {iconId:"expressJs", text:"express Js"},
-    {iconId:"nestJs", text:"nest Js"},
-    {iconId:"docker", text:"docker"},
-    {iconId:"reactJs", text:"react Js"},
-    {iconId:"reactNative", text:"react Native"},
-    {iconId:"styledComponents", text:"styled Components"},
-    {iconId:"redux", text:"redux"},
-    {iconId:"git", text:"git", viewBox:"0 0 32 32"},
-  ]
+    {
+      iconId: "javaScript",
+      text: "Javascript",
+      width: "116",
+      height: "120",
+      viewBox: "0 0 116 120",
+    },
+    { iconId: "typeScript", text: "typescript" },
+    { iconId: "mongoDb", text: "mongo db" },
+    { iconId: "PostgreSql", text: "PostgreSQL" },
+    { iconId: "jest", text: "Jest" },
+    { iconId: "expressJs", text: "express Js" },
+    { iconId: "nestJs", text: "nest Js" },
+    { iconId: "docker", text: "docker" },
+    { iconId: "reactJs", text: "react Js" },
+    { iconId: "reactNative", text: "react Native" },
+    { iconId: "styledComponents", text: "styled Components" },
+    { iconId: "redux", text: "redux" },
+    { iconId: "git", text: "git", viewBox: "0 0 32 32" },
+  ];
 
   return (
-    <S.Skills>
+    <S.Skills id="skills">
       <Title title="Skills" />
       <S.Wrapper>
         {skillsItems.map((s) => {
@@ -32,9 +38,9 @@ export const Skills: React.FC = () => {
               height={s.height}
               viewBox={s.viewBox}
             />
-          )
+          );
         })}
       </S.Wrapper>
     </S.Skills>
   );
-}
+};
