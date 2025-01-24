1ca838b783baabe7a7deb7241bff1b167ea5b4d2
ottolol <alexsveshnik@gmail.com>
2025-01-24 18:50:06 +0300

    remove bottom menu

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import { Icon } from "../../components/icon/Icon";
-import { DesktopMenu } from "../../components/menu/DesktopMenu";
 import { StyledFlexWrapper } from "../../components/FlexWrapper";
-import { S } from "./Footer_Styles"
+import { S } from "./Footer_Styles";
 
-const items = ["Projects", "Contacts"];
 const linksItems = [
-  {href: "#", iconId: "gmail", width: "39", height: "38", viewBox: "0 0 39 38", text: "Gmail"},
-  {href: "#", iconId: "linkein", width: "39", height: "38", viewBox: "0 0 39 38", text: "linkedin"},
-  {href: "#", iconId: "github", width: "39", height: "38", viewBox: "0 0 39 38", text: "GITHUB"}
-]
+  {
+    href: "#",
+    iconId: "gmail",
+    width: "39",
+    height: "38",
+    viewBox: "0 0 39 38",
+    text: "Gmail",
+  },
+  {
+    href: "#",
+    iconId: "linkein",
+    width: "39",
+    height: "38",
+    viewBox: "0 0 39 38",
+    text: "linkedin",
+  },
+  {
+    href: "#",
+    iconId: "github",
+    width: "39",
+    height: "38",
+    viewBox: "0 0 39 38",
+    text: "GITHUB",
+  },
+];
 
 export const Footer: React.FC = () => {
   return (
@@ -19,18 +38,20 @@ export const Footer: React.FC = () => {
           {linksItems.map((l) => {
             return (
               <S.Link href={l.href}>
-                <Icon iconId={l.iconId} width={l.width} height={l.height} viewBox={l.viewBox} />
+                <Icon
+                  iconId={l.iconId}
+                  width={l.width}
+                  height={l.height}
+                  viewBox={l.viewBox}
+                />
                 <span>{l.text}</span>
               </S.Link>
-            )
+            );
           })}
         </S.Wrapper>
-        <S.BottomMenuStyles>
-          <DesktopMenu menuItems={items} className="bottom-menu" />
-        </S.BottomMenuStyles>
+
         <S.Copyright>Web Developer 2025</S.Copyright>
       </StyledFlexWrapper>
     </S.Footer>
   );
-}
-
+};
