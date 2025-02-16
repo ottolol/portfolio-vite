e3da3426a9e2b580604e347171d8811213f60fca
ottolol <alexsveshnik@gmail.com>
2025-02-16 12:43:33 +0300

    update children Popup.tsx

--- src/components/popup/Popup.tsx
+++ src/components/popup/Popup.tsx
@@ -1,15 +1,19 @@
 import { S } from "./Popup_Styles";
 
 type PopupPropsType = {
-  popUpText?: React.ReactNode | string;
+  children: React.ReactNode | string;
   isOpen: boolean;
   onClick?: () => void;
 };
 
-export const Popup: React.FC<PopupPropsType> = (props: PopupPropsType) => {
+export const Popup: React.FC<PopupPropsType> = ({
+  children,
+  isOpen,
+  onClick,
+}: PopupPropsType) => {
   return (
-    <S.PopupWrapper isOpen={props.isOpen} onClick={props.onClick}>
-      {props.popUpText}
+    <S.PopupWrapper isOpen={isOpen} onClick={onClick}>
+      {children}
     </S.PopupWrapper>
   );
 };
